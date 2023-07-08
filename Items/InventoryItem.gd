c4cc951b35c5a5de071d6f967c0cb87cb760931a
Carpenter Blue <114881518+CarpenterBlue@users.noreply.github.com>
2023-07-08 10:33:31 +0200

    morning good morning

--- Items/InventoryItem.gd
+++ Items/InventoryItem.gd
@@ -1,10 +1,22 @@
 extends Control
 class_name InventoryItem
 
-func _gui_input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		var data = get_viewport().set_meta("drag",self)
-		hide()
+export var type := ""
 
-func use(enemy):
-	pass
+func _gui_input(event):
+	on_click(event)
+
+
+func on_click(event):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
+		var data = get_viewport().get_meta("drag",false)
+		
+		if data:
+			if has_method("upgrade"):
+				if data != self and data.type == type:
+					call("upgrade",data)
+					data.queue_free()
+					get_viewport().set_meta("drag",false)
+		else:
+			hide()
+			get_viewport().set_meta("drag",self)
